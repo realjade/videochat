c6024a442892030ecadf28642a02310dc6e19614
jade <zyyjade@gmail.com>
2013-11-25 23:04:49 +0800

    交互图

--- js/apps/mainvideo.js
+++ js/apps/mainvideo.js
@@ -4,7 +4,67 @@ $(function(){
         bgcolor: "#ffffff",
         allowscriptaccess: "sameDomain",
         allowfullscreen: "true"
-    }; 
+    };
+    var chat = new Chat({
+        onConnected:onConnected,
+        onMessage:onMessage,
+        onRoomMessage:onRoomMessage
+    });
+    $('#connect').click(function(){
+        var self = $(this);
+        if(!self.data('logout')){
+            var jid = $('#jid').val(),
+            pass = $('#pass').val();
+            chat.setOptions({jid:jid,pass:pass});
+            chat.connect();
+        }else{
+            self.val('登录');
+            self.data('logout',false);
+            chat.disconnect();
+        }
+    });
+    function onConnected(){
+        var connectBtn = $('#connect');
+        connectBtn.val('注销');
+        connectBtn.data('logout',true);
+        chat.joinRoom('xx@vv.183.203.16.207');
+    }
+    function onMessage(form,msg,type){
+
+    }
+    function onRoomMessage(from,msg,type){
+        message((from ? from : '[消息]') + ': ' + msg);
+    }
+    $('#connectAnonymous').click(function () {
+        $('#jid').val('');
+        $('#pass').val('');
+        $('#connect').trigger('click');
+        $(this).hide();
+    });
+    $('#pass').keypress(function(event){
+        if (event.which == 13) {
+            $('#connect').trigger('click');
+        }
+    });
+    
+    
+    $('#send').bind('click', function () {
+        var button = $('#send').get(0);
+        var to = $('#to').get(0).value;
+        var text = $('#msg').get(0).value;
+        
+        var reply = $msg({to: to, from: connection.jid, type: 'chat'})
+                        .c("body").t(text);
+        connection.send(reply.tree());
+        $('#msg').get(0).value = '';
+
+        message('我 to ' + to.split('@')[0] + ': ' + text);
+    });
+    $('#msg').keypress(function(event){
+        if (event.which == 13) {
+            $('#send').trigger('click');
+        }
+    });
     $('#btnPublish').click(function(ev){
         ev.preventDefault();
         var uid = $('#UID').val();
@@ -35,4 +95,7 @@ $(function(){
             { host: '183.203.16.207', port: 8108, uid: uid }, params, {id:id, name:'Consumer'});
         
     });
+    function message(msg){
+        $('<div>'+msg+'</div>').appendTo($('#message'));
+    }
 });
--- js/lib/chat.js
+++ js/lib/chat.js
@@ -1,150 +1,120 @@
 var BOSH_SERVICE = '/http-bind/'
 var connection = null;
 
-function Chat() {
+function Chat(o) {
+    var options = {
+        host:'183.203.16.207',
+        port:'',
+        bosh_service:'/http-bind/',
+        jid:'',
+        pass:'',
+        onConnected:jQuery.noop,
+        onMessage:jQuery.noop,
+        onRoomMessage:jQuery.noop
+    }
+    $.extend(options,o);
+    this.options = options;
+    this.init();
 }
+Chat.prototype = {
+    init:function(){
+        var self = this;
+        var connection = self.connection = new Strophe.Connection(self.options.bosh_service);
+        // Uncomment the following lines to spy on the wire traffic.
+        connection.rawInput = self.rawInput;
+        connection.rawOutput = self.rawOutput;
 
-function message(msg) 
-{
-    $('#message').append('<div></div>').append(document.createTextNode(msg));
-}
+        // Uncomment the following line to see all the debug output.
+        Strophe.log = self.log;
+    },
+    rawInput:function(data){
+        console.log('Strophe.RECV: ' + data);
+    },
+    rawOutput:function(data){
+        console.log('Strophe.SEND: ' + data);
+    },
+    log:function(level, msg){
+        //console.log('Strophe.LOG: ' + msg);
+    },
+    connect:function(){
+        var self = this;
+        self.connection.connect(self.options.jid ? (self.options.jid + '@' + self.options.host) : self.options.host,
+                               self.options.pass ? self.options.pass : null,
+                               self.onConnect.bind(self));
+    },
+    onConnect:function(status){
+        var self = this;
+        if (status == Strophe.Status.CONNECTING){
+            console.log('connecting.');
+        } else if (status == Strophe.Status.CONNFAIL){
+            console.log('failed to connect.');
+        } else if (status == Strophe.Status.DISCONNECTING){
+            console.log('disconnecting.');
+        } else if (status == Strophe.Status.DISCONNECTED){
+            console.log('disconnected.');
+            self.connect();
+        } else if (status == Strophe.Status.CONNECTED){
+            console.log('connected.');
+            console.log('JID: ' + self.connection.jid);
+            console.log('SID: ' + self.connection.sid);
+            console.log('RID: ' + self.connection.rid);
+            self.options.onConnected.call(self);
+            self.connection.addHandler(self.onMessage, null, 'message', null, null,  null);
+            self.connection.send($pres().tree());
+        }
+    },
+    disconnect:function(){
+        this.connection.disconnect();
+    },
+    send:function(){
 
-function onConnect(status)
-{
-    if (status == Strophe.Status.CONNECTING) {
-        console.log('connecting.');
-    } else if (status == Strophe.Status.CONNFAIL) {
-        console.log('failed to connect.');
-        $('#connect').get(0).value = 'connect';
-    } else if (status == Strophe.Status.DISCONNECTING) {
-        console.log('disconnecting.');
-    } else if (status == Strophe.Status.DISCONNECTED) {
-        $('[name=msessage]').hide();
-        console.log('disconnected.');
-        $('#connect').get(0).value = '登录';
-        connection.connect(connection.jid, $('#pass').get(0).value, onConnect);
-    } else if (status == Strophe.Status.CONNECTED) {
-        $('#connect').get(0).value = '注销';
-        console.log('connected.');
-        console.log('JID: ' + connection.jid);
-        console.log('SID: ' + connection.sid);
-        console.log('RID: ' + connection.rid);
+    },
+    onMessage:function(msg) {
+        var self = this,
+            to = msg.getAttribute('to'),
+            from = msg.getAttribute('from'),
+            type = msg.getAttribute('type'),
+            elems = msg.getElementsByTagName('body');
 
-        connection.addHandler(onMessage, null, 'message', null, null,  null); 
-        connection.send($pres().tree());
-        
-        $('[name=msessage]').show();
+        if (type == "chat" && elems.length > 0) {
+            var body = elems[0];
+            self.options.onMessage.call(self,from,Strophe.getText(body),type);
+        }
 
+        // we must return true to keep the handler alive.
+        // returning false would remove it after it finishes.
+        return true;
+
+    },
+    joinRoom:function(roomId){
+        var self = this;
         // connect room
-        connection.muc.join(
-            'xx@vv.183.203.16.207', 
-            connection.jid.split('@')[0], 
-            onRoomMessage, onPresence, onRoster);
-    }
-}
-
-function onMessage(msg) {
-    var to = msg.getAttribute('to');
-    var from = msg.getAttribute('from');
-    var type = msg.getAttribute('type');
-    var elems = msg.getElementsByTagName('body');
-
-    if (type == "chat" && elems.length > 0) {
-        var body = elems[0];
-
-        message('[私聊]' + from.split('@')[0] + ': ' +
-            Strophe.getText(body));
-            
-        console.log('get message from ' + from);
-    }
-
-    // we must return true to keep the handler alive.
-    // returning false would remove it after it finishes.
-    return true;
-}
-
-function onRoomMessage(stanza, room) {
-    var from = stanza.getAttribute('from').split('/')[1];
-    var type = stanza.getAttribute('type');
-    var elems = stanza.getElementsByTagName('body');
-    
-    message((from ? from : '[消息]') + ': ' + 
-            Strophe.getText(elems[0]));
-
-    return true;
-}
-
-function onPresence(stanza, room) {
-    console.log('onPresence stanza: ' + stanza);
-    console.log('onPresence room  : ' + room);
-    
-    return true;
-}
-
-function onRoster(roster, room) {
-    console.log('onPresence roster: ' + roster);
-    console.log('onPresence room  : ' + room);
-    
-    return true;
-}
-
-$(function () {
-    $('[name=msessage]').hide();
-
-    connection = new Strophe.Connection(BOSH_SERVICE);
-
-    // Uncomment the following lines to spy on the wire traffic.
-    connection.rawInput = function (data) { console.log('Strophe.RECV: ' + data); };
-    connection.rawOutput = function (data) { console.log('Strophe.SEND: ' + data); };
-
-    // Uncomment the following line to see all the debug output.
-    Strophe.log = function (level, msg) { console.log('Strophe.LOG: ' + msg); };
-
-
-    $('#connect').bind('click', function () {
-        var button = $('#connect').get(0);
-        if (button.value == '登录') {
-            button.value = '注销';
-            
-            var jidField = $('#jid').get(0);
-            var passField = $('#pass').get(0);
-
-            connection.connect(jidField.value == '' ? '183.203.16.207' : jidField.value + '@183.203.16.207',
-                               jidField.value == '' ? null : passField.value,
-                               onConnect);
-        } else {
-            button.value = '登录';
-            connection.disconnect();
-        }
-    });
-    $('#connectAnonymous').bind('click', function () {
-        $('#jid').get(0).value = '';
-        $('#pass').get(0).value = '';
-        $('#connect').trigger('click');
-        $(this).hide();
-    });
-    $('#pass').keypress(function(event){
-        if (event.which == 13) {
-            $('#connect').trigger('click');
-        }
-    });
-    
-    
-    $('#send').bind('click', function () {
-        var button = $('#send').get(0);
-        var to = $('#to').get(0).value;
-        var text = $('#msg').get(0).value;
+        self.connection.muc.join(
+            roomId,
+            self.connection.jid.split('@')[0], 
+            self.onRoomMessage.bind(self),self.onPresence.bind(self), self.onRoster.bind(self));
+    },
+    onRoomMessage:function(stanza, room) {
+        var self = this,
+            from = stanza.getAttribute('from').split('/')[1],
+            type = stanza.getAttribute('type'),
+            elems = stanza.getElementsByTagName('body');
+        self.options.onRoomMessage.call(self,from,Strophe.getText(elems[0]),type);
+        return true;
+    },
+    onPresence:function(stanza, room) {
+        console.log('onPresence stanza: ' + stanza);
+        console.log('onPresence room  : ' + room);
+        return true;
+    },
+    onRoster:function(roster, room) {
+        console.log('onPresence roster: ' + roster);
+        console.log('onPresence room  : ' + room);
         
-        var reply = $msg({to: to, from: connection.jid, type: 'chat'})
-                        .c("body").t(text);
-        connection.send(reply.tree());
-        $('#msg').get(0).value = '';
+        return true;
+    },
+    setOptions:function(o){
+        $.extend(this.options,o);
+    }
 
-        message('我 to ' + to.split('@')[0] + ': ' + text);
-    });
-    $('#msg').keypress(function(event){
-        if (event.which == 13) {
-            $('#send').trigger('click');
-        }
-    });
-});
+};
